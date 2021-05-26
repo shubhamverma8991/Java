11e89e55fd8a37214176ecfb3b43371b6065d4cd
Shubham <66171089+shubhamverma8991@users.noreply.github.com>
2021-05-26 15:24:39 +0530

    Add files via upload

---
+++ 10thMay_Day4/Q3.java
@@ -0,0 +1,13 @@
+//Consider that payroll software needs to be developed for computerization of
+//operations of an ABC organization. The organization has employees.
+public class Q3 
+//Main Class
+{
+	public static void main(String[] args) {
+		Q3_1 emp =new Q3_1();
+		emp.accept();
+		emp.display();
+	}
+}
+
+
