5e1669c3f016a62dd08aef8f25fdc5cca65429c2
Shubham <66171089+shubhamverma8991@users.noreply.github.com>
2021-05-26 15:28:47 +0530

    Add files via upload

---
+++ 10thMay_Day4/Create Date class with  data members day,month,year/Main.java
@@ -0,0 +1,14 @@
+//Create Date class with  data members day,month,year.
+//Write a method to accept all data members.write display function to display date.
+
+
+public class Q1 {
+	//Main Class
+	public static void main(String[] args) {
+		Q1_1 calender= new Q1_1 ();
+		calender.accept();
+		calender.display();	
+	}
+
+}
+
